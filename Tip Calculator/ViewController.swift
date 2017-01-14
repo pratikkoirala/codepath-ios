40be53fbba656cceedeaa44da0dfb0e4d9bb70b6
Pratik Koirala <pratik@Pratiks-MacBook-Pro.local>
2017-01-14 02:32:48 -0500

    Create basic tip calculator

--- Tip Calculator/ViewController.swift
+++ Tip Calculator/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var chooseTipPercent: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,25 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    
+    @IBAction func calculateTipAmount(_ sender: AnyObject) {
+        
+        let availablePercentages = [0.15, 0.20, 0.25]
+        // Calculate tip and total
+        let bill = Double(billField.text!) ?? 0
+        let tip = availablePercentages[chooseTipPercent.selectedSegmentIndex] * bill
+        let total = bill + tip
+        
+        // Update the display with the calculated amounts
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+ 
+    // Remove keyboard display when tapped outside the edit box
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+ 
 }
 
